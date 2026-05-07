c3b8c7aa119af1b4289db5a22feb45116d291ef3
rogeriorodrigues1970 <6x4vzm9zb8@privaterelay.appleid.com>
2026-05-07 11:20:56 -0300

    Add files via upload

---
+++ catalogo_pausa_plena.js
@@ -0,0 +1,24 @@
+// Catálogo Pausa Plena — gerado em 2026-05-07
+window.__catalogoPP = [
+  {
+    "id": 8,
+    "nome": "Difusor Lichia",
+    "cat": "Difusor",
+    "valor": 86.35,
+    "tamanho": "350ml"
+  },
+  {
+    "id": 9,
+    "nome": "Home Spray Lichia",
+    "cat": "Home Spray",
+    "valor": 70.54,
+    "tamanho": "350ml"
+  },
+  {
+    "id": 10,
+    "nome": "Vela Margarida",
+    "cat": "Velas Aromáticas",
+    "valor": 7.09,
+    "tamanho": "1un"
+  }
+];
